e5b40c68b2bd07c8c7efb616d54474668d5dd8f7
guyalt11 <guyalt11@gmail.com>
2026-02-21 11:02:30 +0100

    Added chess board to ui

--- App.tsx
+++ App.tsx
@@ -1,31 +1,38 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import {
   StyleSheet,
-  Text,
   View,
   StatusBar,
+  SafeAreaView,
 } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import Chessboard, { ChessboardRef } from 'react-native-chessboard';
 
 function App(): React.JSX.Element {
+  const chessboardRef = useRef<ChessboardRef>(null);
+
   return (
-    <View style={styles.container}>
-      <StatusBar barStyle="dark-content" />
-      <Text style={styles.text}>Helloo World</Text>
-    </View>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaView style={styles.container}>
+        <StatusBar barStyle="dark-content" />
+        <View style={styles.boardContainer}>
+          <Chessboard ref={chessboardRef} />
+        </View>
+      </SafeAreaView>
+    </GestureHandlerRootView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  text: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: '#333333',
+  boardContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10,
   },
 });
 
